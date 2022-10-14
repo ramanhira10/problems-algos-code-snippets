a69ffe27d1b67a40a117c0fc092b40e2f3690d4b
Raman Hira <ramanhira@gmail.com>
2022-10-14 22:20:14 +0530

    updated memoization exercise

--- memoization.js
+++ memoization.js
@@ -1,6 +1,5 @@
 /**
  * Memoization is an optimization technique that can be used to reduce time-consuming calculations by saving previous input to something called cache and returning the result from it.
- * 
  */
 
 let sum = 0;
