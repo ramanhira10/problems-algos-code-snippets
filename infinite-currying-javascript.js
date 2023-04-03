684df5a8bb1d93044dd09d5478b720a42085e662
Raman Hira <ramanhira@gmail.com>
2023-04-03 13:40:18 +0530

    updated the code for currying

--- infinite-currying-javascript.js
+++ infinite-currying-javascript.js
@@ -13,3 +13,28 @@ function add (a) {
 }
 
 console.log(add(1)(2)());
+
+
+function curry (func) {
+
+    return function curried (...args) {
+        if (args.length >= func.length) {
+            return func.apply(this, args);
+        } else {
+            return function (...args2) {
+                return curried.apply(this, args.concat(args2));
+            }
+        }
+    };
+}
+
+
+function sum (a, b, c) {
+    return a + b + c;
+}
+
+const curriedSum = curry(sum);
+
+console.log(curriedSum(1, 2, 3));
+console.log(curriedSum(1)(2, 3));
+console.log(curriedSum(1)(2)(3));
