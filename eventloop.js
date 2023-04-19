7eb6cade064d7bb5ad4fce24d5d72dc522985c7a
Raman Hira <ramanhira@gmail.com>
2023-04-19 13:45:03 +0530

    added the event loop exercise

--- eventloop.js
+++ eventloop.js
@@ -1,1 +1,17 @@
-console.log('event looping');
+/**
+ * Macro Task: setTimeout
+ * 
+ * Micro Task: .then()
+ * 
+ * task: console.log
+ * 
+ * first it execute the tasks then micro tasks and then look and execute the macro tasks
+ */
+
+
+setTimeout(() => console.log('Timeout'));
+
+Promise.resolve().then(() => console.log('Promise resolved'));
+
+console.log('code execution finished')
+
