42cb32e68653ef61679b582c7577bdd299472abb
Raman Hira <ramanhira@gmail.com>
2023-05-04 12:56:46 +0530

    added an exercise on html and js

---
+++ ps-test/js/script.js
@@ -0,0 +1,8 @@
+/*
+Create a vanilla JS application that consumes the https://pokeapi.co/api/v2/pokemon/ API and displays a dropdown list of pokemons. When the user selects a pokemon from the dropdown, the application should make a second API call using the "url" returned in the first API call to fetch the pokemon's details, including its abilities, and display them on the page. Once the details are fetched, they should be cached and the application should not make another API call for that pokemon again.						
+*/
+
+
+window.onload = function () {
+    console.log('prepare a solution from here');
+};
