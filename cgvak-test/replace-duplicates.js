5d4c0220561d19ef49f06b280013b2187d99a4d9
Raman Hira <ramanhira@gmail.com>
2023-05-04 21:33:51 +0530

    added a replace duplicate exercise on javascript

---
+++ cgvak-test/replace-duplicates.js
@@ -0,0 +1,33 @@
+/**
+ * 
+ * input = [1, 3, 3, 4, 6, 9, 9, 9, 12, 18];
+ * 
+ Expected output = [1, 3, 4, 6, 9, 12, 18, 0, 0, 0];
+
+    - Array should be traversed only once
+    - Should not use new Array, List etc
+    - can use indexes
+    - Array index order should be maintained
+ */
+
+const input = [1, 3, 3, 4, 6, 9, 9, 9, 12, 18];
+
+const replaceDuplicates = inputArr => {
+    let outputArray = [];
+    const maskedElements = [];
+
+    inputArr.forEach(eachElement => {
+        if(outputArray.indexOf(eachElement) === -1) {
+            outputArray.push(eachElement)
+        } else {
+            maskedElements.push(0);
+        }
+    });
+
+    return [...outputArray.sort((a, b) => a - b), ...maskedElements];
+};
+
+const maskedArray = replaceDuplicates(input);
+
+console.log(input);
+console.log(maskedArray);
