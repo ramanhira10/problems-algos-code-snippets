80a84415998b96e82025fe411f803bc7b71013f1
Raman Hira <ramanhira@gmail.com>
2023-05-12 19:58:50 +0530

    added an exercise for getting the location in javascript

---
+++ user-location/js/script.js
@@ -0,0 +1,16 @@
+const button = document.querySelector('button');
+
+button.addEventListener('click', () => {
+    navigator.geolocation.getCurrentPosition(position => {
+        const { latitude, longitude } = position.coords;
+        const url = `https://nominatim.openstreetmap.org/reverse?format=json&lat=${latitude}&lon=${longitude}`;
+        fetch(url)
+            .then(res => res.json())
+            .then(data => {
+                console.table(data.address);
+            }).catch(() => {
+                console.log("Error fetching data from API")
+            });
+
+    });
+});
