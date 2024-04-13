96df06b706eab7d7cf87c629a0613856b926dcab
Raman Hira <ramanhira@gmail.com>
2024-04-13 13:54:28 +0530

    implemented the quick sort algoithm in javascript

---
+++ quick-sort.js
@@ -0,0 +1,25 @@
+const quickSort = (arr) => {
+  if (arr.length <= 1) {
+    return arr;
+  }
+
+  let pivot = arr[0];
+  let leftArr = [];
+  let rightArr = [];
+
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] < pivot) {
+      leftArr.push(arr[i]);
+    } else {
+      rightArr.push(arr[i]);
+    }
+  }
+
+  return [...quickSort(leftArr), pivot, ...quickSort(rightArr)];
+};
+
+const arrayToSort = [3, 2, 10, 9, 7, 8];
+
+const arrayAfterSort = quickSort(arrayToSort);
+
+console.log(arrayAfterSort);
